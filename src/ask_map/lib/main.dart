070a405858afad21e1aad08550d5e434606d1ee8
OMEGA <lightsound2321@gmail.com>
2024-10-16 02:02:42 +0900

    ref: removed unnecessary code

--- src/ask_map/lib/main.dart
+++ src/ask_map/lib/main.dart
@@ -2,23 +2,27 @@ import 'package:flutter/material.dart';
 import 'package:ask_map/view/screens/MapScreen.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const MaterialApp(
+    home: MapScreen(
+      title: 'MapScreen',
+    ),
+  ));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MapScreen(
-        title: 'MapScreen',
-      ),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Flutter Demo',
+//       theme: ThemeData(
+//         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+//         useMaterial3: true,
+//       ),
+//       home: const MapScreen(
+//         title: 'MapScreen',
+//       ),
+//     );
+//   }
+// }
